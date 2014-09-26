8b2c1dda5e4d0ef3fac4b76065852f69eae7a2e7
Juan Antonio Macías <juanmtg@gmail.com>
2014-09-26 16:57:56 +0200

    Added fractalapi module to store default values and functions to compute the sets.

---
+++ fractalapi.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from pylab import *
+from fractalutils import *
+
+
+class WxPythonUtil():
+    def __init__(self):
+        pass
+
+
+class JuliaApi():
+    def __init__(self):
+        self.xmin = -2
+        self.xmax = 2
+        self.ymin = -1.5
+        self.ymax = 1.5
+        self.densidad = 500
+        self.maxiter = 100
+
+    def juliaimage(self, func, c):
+        xg, yg = meshgrid(linspace(self.xmin, self.xmax, self.densidad),
+                          linspace(self.ymax, self.ymin, self.densidad))
+        iters = zeros((self.densidad, self.densidad))
+        z = xg + 1j*yg
+
+        for n in xrange(self.maxiter):
+            indic = (abs(z) <= 10)
+            z[indic] = feval(func, self, z[indic], c)
+            iters[indic] = n
+
+        return iters
+
+
+class MandelbrotApi():
+    def __init__(self):
+        self.xmin = -2.5
+        self.xmax = 1.5
+        self.ymin = -1.5
+        self.ymax = 1.5
+        self.densidad = 500
+        self.maxiter = 100
+
+    def mandimage(self, func):
+        xg, yg = meshgrid(linspace(self.xmin, self.xmax, self.densidad),
+                          linspace(self.ymax, self.ymin, self.densidad))
+        iters = zeros((self.densidad, self.densidad))
+        c = xg + 1j*yg
+
+        z = zeros_like(c)
+        for n in xrange(self.maxiter):
+            indic = (abs(z) <= 10)
+            z[indic] = feval(func, self, z[indic], c[indic])
+            iters[indic] = n
+
+        return iters
+
+if __name__ == '__main__':
+    pass
