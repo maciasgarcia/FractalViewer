fda28e736488df02c1a73a5169fee29c1ca7655b
Juan Antonio Macías <juanmtg@gmail.com>
2014-10-04 17:12:54 +0200

    Added splashscreen, temporal placeholder.

--- main.py
+++ main.py
@@ -3,10 +3,27 @@ import wx
 from gui import *
 
 
+class SplashScreen(wx.SplashScreen):
+    """
+Create a splash screen widget.
+    """
+    def __init__(self, parent=None):
+        # This is a recipe to a the screen.
+        # Modify the following variables as necessary.
+        aBitmap = wx.Image(name = "images/splashscreen.png").ConvertToBitmap()
+        splashStyle = wx.SPLASH_CENTRE_ON_SCREEN | wx.SPLASH_TIMEOUT
+        splashDuration = 1000 # milliseconds
+        # Call the constructor with the above arguments in exactly the
+        # following order.
+        wx.SplashScreen.__init__(self, aBitmap, splashStyle,
+                                 splashDuration, parent)
+
+        wx.Yield()
+
 class MainFrame(wx.Frame):
     def __init__(self):
         wx.Frame.__init__(self, None, title="Fractales", size=(1000, 700))
-
+        self.Center()
         # Created Panel and Notebook.
         p = wx.Panel(self)
         nb = wx.Notebook(p)
@@ -26,8 +43,15 @@ class MainFrame(wx.Frame):
         sizer.Add(nb, 1, wx.EXPAND)
         p.SetSizer(sizer)
 
+class MainApp(wx.App):
+    def OnInit(self):
+        MySplash = SplashScreen()
+        MySplash.Show()
+        MainFrame().Show()
+
+        return True
+
 
 if __name__ == "__main__":
-    app = wx.App()
-    MainFrame().Show()
+    app = MainApp()
     app.MainLoop()
